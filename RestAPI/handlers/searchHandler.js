303611526d11bc42d0aea99f1db849ef7d704e4c
ludwinrivera <ludwin.rivera@jalasoft.com>
2015-10-08 13:02:01 -0300

    Define structure

---
+++ RestAPI/handlers/searchHandler.js
@@ -0,0 +1,16 @@
+﻿
+/*
+ * Search Controller.
+ */
+var dbManager = require('../data_access/DBManager.js');
+
+module.exports = {
+    getKeyWordList: function (key) {
+        var query = "Select * From Prod";
+//        var query = "Select * From Prod Where SKU = '" + key + "'";
+        var result = dbManager.executeQuery(query);
+        
+        return result;
+    }
+   
+};
---
+++ RestAPI/routes/SearchAPI.js
@@ -0,0 +1,22 @@
+﻿
+/*
+ * Search API.
+ */
+var searchHandler = require('../handlers/searchHandler.js');
+
+//Routes
+app.get('/searchapi/:key', getKeyWordList);
+//app.get('/searchapi/', getKeyWordList);
+
+//Functions
+module.exports = function (app) {
+    
+    getKeyWordList = function (req, res) {
+        var result = searchHandler.getKeyWordList(req.params.key);
+        //parse result to xml
+        res.send("Key: " + req.params.key + "\n" + result);
+    }
+    //getAll
+    
+    
+};
